5d3a082ba79b65597bb6a322c82741c3a24b21a9
clipwww <clipwww@gmail.com>
2019-01-24 16:46:22 +0800

    feat: add GA

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="profile-card">
     <div class="profile-card__content">
+      <div class="profile-card__picture">
+        <img src="https://placehold.it/500x500" alt="">
+      </div>
       clipwww.github.io
       <h1>施工中</h1>
     </div>
@@ -22,10 +25,28 @@ export default class Profile extends Vue {}
   padding: 0 15px;
 
   &__content {
+    position: relative;
     background-color: $black;
     color: #fff;
     padding: 10px;
     box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+    @include respond-to(sm) {
+      padding-top: 70px;
+    }
+  }
+
+  &__picture {
+    position: absolute;
+    right: 15px;
+    top: -50px;
+    width: 150px;
+
+    @include respond-to(sm) {
+      right: initial;
+      left: 50%;
+      transform: translateX(-50%);
+      top: -100px;
+    }
   }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import '@/assets/css/normalize.css';
+import '@/assets/scss/main.scss';
 
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
 import './registerServiceWorker';
+import '@/plugins/vue-analytics';
 
 Vue.config.productionTip = false;
 
